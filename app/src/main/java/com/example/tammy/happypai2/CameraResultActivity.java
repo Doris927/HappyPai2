237db040003f1553c295397391faefc351908332
tammytangg <tammytangg@gmail.com>
2017-07-01 14:03:46 +0900

    7/1 图片保存

--- app/src/main/java/com/example/tammy/happypai2/CameraResultActivity.java
+++ app/src/main/java/com/example/tammy/happypai2/CameraResultActivity.java
@@ -1,16 +1,32 @@
 package com.example.tammy.happypai2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.example.tammy.happypai2.util.Util;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraResultActivity extends AppCompatActivity {
+
+    ImageButton bt_cancel, bt_sure;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +34,12 @@ public class CameraResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera_result);
 
         String path = getIntent().getStringExtra("picPath");
-        ImageView imageView = (ImageView)findViewById(R.id.picResult);
+        ImageView imageView = (ImageView)findViewById(R.id.iv_result);
         try {
             FileInputStream fis = new FileInputStream(path);
-            Bitmap bitmap = BitmapFactory.decodeStream(fis);
+            bitmap = BitmapFactory.decodeStream(fis);
             Matrix matrix = new Matrix();
-            matrix.setRotate(270);
+            matrix.setRotate(90);
             bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
             imageView.setImageBitmap(bitmap);
 
@@ -32,5 +48,43 @@ public class CameraResultActivity extends AppCompatActivity {
         }
 //        Bitmap bitmap = BitmapFactory.decodeFile(path);
 //        imageView.setImageBitmap(bitmap);
+
+        initView();
+    }
+
+    private void initView(){
+        bt_cancel = (ImageButton)findViewById(R.id.result_bt_cancel);
+        bt_sure = (ImageButton) findViewById(R.id.result_bt_sure);
+
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        //保存图片
+        bt_sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Util.saveImageToGallery(getApplicationContext(),bitmap);
+                dialog();
+            }
+        });
+    }
+
+    private void dialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Saved successfully!");
+        builder.setTitle("Info");
+        builder.setPositiveButton("Sure", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        builder.create().show();
     }
 }
